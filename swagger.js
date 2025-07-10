fcbb8514fa7c2f0e3a1c597d2204d5d794e66f56
SagarJ95 <sagarjagade2023@gmail.com>
2025-07-10 17:49:15 +0530

    swagger update

--- swagger.js
+++ swagger.js
@@ -6,10 +6,21 @@ const doc = {
         title:"keep in basket",
         description:"Description"
     },
-    host:"localhost:3848"
+    host:"localhost:3848",
+    schemes: ["http"],
+    tags: [
+        {
+        name: "Web API",
+        description: "Public-facing endpoints"
+        },
+        {
+        name: "Admin API",
+        description: "Admin panel endpoints"
+        }
+    ]
 };
 
 const outputFile = "./swagger-output.json";
-const routes = ['./routes/web.js'];
+const routes = ['./routes/web.js','./routes/admin_api.js'];
 const swagger = swaggerAutogen();
 swagger(outputFile,routes,doc)
